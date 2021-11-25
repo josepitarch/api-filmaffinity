d16fd045a4218c203f21acfab5eec2c85f0b6c40
Jopimi <josepitarch98@gmail.com>
2021-11-25 12:47:11 +0100

    Arreglada la llamada a la función metadataFilm

--- routes/search-film.js
+++ routes/search-film.js
@@ -12,7 +12,7 @@ router.get('/', async function(req, res) {
 
         await page.goto(`https://www.filmaffinity.com/es/search.php?stext=${name}`)
 
-        const search = await page.evaluate(() => {
+        let search = await page.evaluate(async() => {
             response = []
             const years = document.querySelectorAll('.ye-w')
             const films = document.querySelectorAll('.movie-card')
@@ -36,7 +36,12 @@ router.get('/', async function(req, res) {
             }
 
             return response
+
         });
+
+        if (search.length === 0) {
+            search = await metadataFilm(425311)
+        }
 
         res.json(search)
 
